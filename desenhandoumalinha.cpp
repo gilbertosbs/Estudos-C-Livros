b1f32e8039f11251ef3a54ce70459dad491c6997
gilberto aparecido pereira <49577657+gilbertosbs@users.noreply.github.com>
2020-07-17 23:05:00 -0300

    Add files via upload

---
+++ desenhandoumalinha.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main(void)
+{
+     for( int lin=1; lin < 25; lin++)
+     {
+          for (int col = 1; col < 25; col ++)
+               if(lin == col)
+                    cout << '\xDB';
+               else
+                    cout << '\xB0';
+          cout << endl;
+     }
+cin.get();
+}
