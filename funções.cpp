ece78bb0d26538d2f24dc12c0a1f20fa2a5686c4
gilberto aparecido pereira <49577657+gilbertosbs@users.noreply.github.com>
2020-07-18 21:31:58 -0300

    Add files via upload

---
+++ funções.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <iomanip>
+int celsius (int fahr);
+using namespace std;
+int main()
+{
+     int c, f;
+     cout << "Digite a temperatura em graus Fahrenheit: ";
+     cin >> f;
+
+     c = celsius(f); //chamada à função
+
+     cout << "Celsius = " << c << endl;
+
+     cin.get();
+
+}
+//celsius ()
+// definição da função
+int celsius( int fahr)
+{
+     int c;
+     c = (fahr - 32) * 5/9;
+     return c;
+}
